ba1dc67671b9a765d0f6a031b59074c8c1c82287
Kewinrodick <rodickkewin@gmail.com>
2024-10-16 00:17:29 +0530

    changed 2

--- Random number/index.js
+++ Random number/index.js
@@ -1,36 +1,45 @@
-let min = 1;
-let max = 100;
+// let min = 1;
+// let max = 100;
 
-let number = Math.floor(Math.random()*(max-min+1))+min;
+// let number = Math.floor(Math.random()*(max-min+1))+min;
 
-let guess;
-let running = true;
-let attempts=0;
+// let guess;
+// let running = true;
+// let attempts=0;
 
-while(running){
+// while(running){
     
-    guess = window.prompt(`Enter the Guess [${min}-${max}]`);
+//     guess = window.prompt(`Enter the Guess [${min}-${max}]`);
     
-    guess = Number(guess); 
-    if(isNaN(guess)){
-        window.alert("INVALID GUESS!");
-    }
-    else if(guess > max || guess < min){
-        window.alert(`INVALID GUESS [${min}-${max}]!`);
-    }
-    else{
-        attempts++;
-         if (guess > number){
-            window.alert("TOO HIGH!");
-        }
-        else if(guess < number){
-            window.alert("TOO LOW!");
-        }
-        else{
-            window.alert(`You Guessed Right!! attempts : ${attempts}`);
-            running=false;
-        }
- 100
-    } 
+//     guess = Number(guess); 
+//     if(isNaN(guess)){
+//         window.alert("INVALID GUESS!");
+//     }
+//     else if(guess > max || guess < min){
+//         window.alert(`INVALID GUESS [${min}-${max}]!`);
+//     }
+//     else{
+//         attempts++;
+//          if (guess > number){
+//             window.alert("TOO HIGH!");
+//         }
+//         else if(guess < number){
+//             window.alert("TOO LOW!");
+//         }
+//         else{
+//             window.alert(`You Guessed Right!! attempts : ${attempts}`);
+//             running=false;
+//         }
+//  100
+//     } 
+// }
+
+const click = document.getElementById("roll");
+const display = document.getElementById("dicelabel");
+let dice;
+
+click.onclick=function(){
+    dice = Math.floor((Math.random()*6))+1;
+    display.textContent=dice;
 }
 
